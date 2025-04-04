afdc30e0605a2321c278ebd146de897b91b7537c
Naheel <96335640+naheelkk@users.noreply.github.com>
2025-04-04 06:11:36 +0000

    gg

--- 04-04-25/remove-digits.sh
+++ 04-04-25/remove-digits.sh
@@ -6,4 +6,4 @@ read -p "Enter file to store the edited: " nfile
 
 sed "s/[0-9]//g" $file > $nfile
 
-cat $nfile
+cat $nfile                                                                                                                                                                                                                                                                                                                                                                                               
